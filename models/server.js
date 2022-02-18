1152a7d8d03e41a23a6c0e37d7923bb51e0a8625
sebastiancortina <sebastiancortina97@gmail.com>
2022-02-18 00:00:15 -0500

    Modelo de producto y rutas

--- models/server.js
+++ models/server.js
@@ -14,6 +14,7 @@ class Server{
         this.paths = {
             auth: '/api/auth',
             categorias: '/api/categorias',
+            productos: '/api/productos',
             usuario: '/api/usuarios'
         }
 
@@ -48,6 +49,7 @@ class Server{
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth'));
         this.app.use(this.paths.usuario, require('../routes/user'));
+        this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
     }
 

ce96d5a14c00207916087dc4e4527a439e230d89
sebastiancortina <sebastiancortina97@gmail.com>
2022-02-21 22:36:37 -0500

    Buscar en otras colecciones

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -7,11 +7,12 @@ const usuario = require('../models/usuario');
 
 const colecionesPermitidas = [
     'usuarios',
-    'categoria',
+    'categorias',
     'productos',
     'roles'
 ];
 
+// Buscar Usuario
 const buscarUsuario = async ( termino = '', res = response ) => {
 
     // Valida si existe el id en mogo 
@@ -27,16 +28,6 @@ const buscarUsuario = async ( termino = '', res = response ) => {
 
     }
 
-
-
-    /*else{
-        res.json({
-            // Si el usuario no exite devuelve un arreglo vacio 
-            results: []
-        });
-
-    }*/
-
     const regex = new RegExp( termino, 'i');
 
     const usuarios = await Usuario.find({ 
@@ -47,9 +38,63 @@ const buscarUsuario = async ( termino = '', res = response ) => {
     res.json({
         results: usuarios
     });
+}
+
+// Busca Categoria
+const buscarCategoria = async ( termino = '', res = response ) => {
+
+    // Valida si existe el id en mogo 
+    const esMongoId = ObjectId.isValid( termino ); // True 
+    
+    if( esMongoId ) {
+        // Devuelve un objeto del usuarario que se desea buscar 
+        const categorias = await Categoria.findById(termino);
+
+        res.json({
+            results:  (categorias) ? [ categorias ] : []
+        });
+
+    }
+
+    const regex = new RegExp( termino, 'i');
+    const categorias = await Categoria.find({ nombre: regex, estado: true});
+
+    res.json({
+        results: categorias
+    });
    
 
 }
+
+// Buscar Producto
+const buscarProducto = async ( termino = '', res = response ) => {
+
+    // Valida si existe el id en mogo 
+    const esMongoId = ObjectId.isValid( termino ); // True 
+    
+    if( esMongoId ) {
+        // Devuelve un objeto del usuarario que se desea buscar 
+        const producto = await Producto.findById(termino).populate('categoria','nombre');
+
+        res.json({
+            results:   (producto) ? [ producto ] : []
+        });
+
+    }
+
+    const regex = new RegExp( termino, 'i');
+    const productos = await Producto.find({ nombre: regex, estado: true});
+
+    res.json({
+        results: productos
+    });
+   
+
+}
+
+
+
+
 
 
 const buscar = ( req, res = response) => {
@@ -67,10 +112,12 @@ const buscar = ( req, res = response) => {
             buscarUsuario(termino, res);
             break;
 
-        case 'categoria':
+        case 'categorias':
+            buscarCategoria(termino, res);
             break;
 
         case 'productos':
+            buscarProducto(termino, res);
             break;
         
         default: 

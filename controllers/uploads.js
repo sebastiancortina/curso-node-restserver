01dd4296ef6f4803861c745b28740bbae0c09375
sebastiancortina <sebastiancortina97@gmail.com>
2022-03-10 23:21:24 -0500

    Sin terminar

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -107,53 +107,8 @@ const mostrarImagen = async (req, res = response ) => {
 }
 
 const actualizarImagenCloudinary = async (req, res = response ) => {
-
-    const {id, coleccion } = req.params;
-
-    let modelo;
-
-    switch (coleccion){
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if (!modelo) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${id}`
-                });
-            }
-        break;
-
-        case 'productos':
-            modelo = await Producto.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un producto con el id ${id}`
-                });
-            }
-        break; 
-
-        default:
-            return res.status(500).json({ msg: 'Se me olvido validar esto'});       
-    }
-
-    // Limpiar imagenes previas
-    if( modelo.img){
-      
-    }
-
-    const { tempFilePath } = req.files.archivo
-    const resp = await cloudinary.uploader.upload( tempFilePath );
-
-    res.json( resp );
-
-    //cloudinary.uploader.upload( )
     
-    /*
-    const nombre = await subirArchivo(req.files, undefined, coleccion);
-    modelo.img = nombre;
-
-    await modelo.save();
-
-    res.json(modelo);*/
+   
 }
 
 
